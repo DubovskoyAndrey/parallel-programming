efc6c780f6f7a28103e5f57e00ff8d0a817eac27
DubovskoyAndrey <32214387+DubovskoyAndrey@users.noreply.github.com>
2018-10-26 21:22:17 +0300

    Дубовской Андрей- Задание №1 (Вариант №2)
    Дубовской Андрей- Задание №1 (Вариант №2)

--- 1606-1/dubovskoy_av/Main.cpp
+++ 1606-1/dubovskoy_av/Main.cpp
@@ -8,10 +8,10 @@
 #include "mpi.h"
 
 using namespace std;
- 
 
 void CreateVec(float *vec, int N)
 {
+	cout << endl;
 	cout << "Vector: ";
 	srand(time(nullptr));
 	for (int i = 0; i < N; i++)
@@ -19,85 +19,101 @@ void CreateVec(float *vec, int N)
 		vec[i] = rand() % 201 - 100;
 		cout << vec[i] << " ";
 	}
-	cout << endl;
+	cout << endl << endl;
 }
-
 
 int main(int argc, char* argv[])
 {
-	int N;
-	cout << "Enter the number of elements" << endl;
-	cin >> N;
-
+	int N=8;
+	
 	float *vec = new float[N];
 
 	float Sum, TotalSum = 0;
 	int  ProcRank, ProcNum;
 	Sum = 0;
 	TotalSum = 0;
-
+	double time1, time2, time3, time4;
 	MPI_Status Status;
+
 	// Инициализация
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	cout << "ProcNum: " << ProcNum << endl;
-	cout << "ProcRank: " << ProcRank << endl;
 	
-	float time1, time2;
+	if (N < ProcNum)
+	{
+		cout << "N < ProcNum";
+		return -1;
+	}
+	
 
-	int n = N / ProcNum;
+	int n = N / (ProcNum-1);
 	float *tmp = new float[n];
 
-	if (ProcRank==0)
+	if (ProcRank == 0)
 	{
+		cout << "ProcNum: " << ProcNum << endl;
 		cout << "ProcRank: " << ProcRank << endl;
 		CreateVec(vec, N);// Инициализвация вектора
-
-		time1 = MPI_Wtime();
 		
-		for (int i = 0; i < ProcNum - 1; i++)
+		//LINER
+		time3 = MPI_Wtime();
+		for (int i = 0; i < N; i++)
+		{
+			TotalSum += vec[i];
+		}
+		time4 = MPI_Wtime();
+		cout << "LinerRunTime: " << time4 - time3 << endl << "Result: " << TotalSum/N << endl<< endl;
+		TotalSum = 0;
+
+
+		//PARALLEL
+		time1 = MPI_Wtime();
+
+		for (int i = 0; i < ProcNum-1; i++)
 		{
 			tmp = vec + i * n;
-			MPI_Send(tmp, n, MPI_FLOAT, i + 1, 0, MPI_COMM_WORLD);
+			MPI_Send(tmp, n, MPI_FLOAT, i+1, 0, MPI_COMM_WORLD);
 		}
 		
 		for (int i = (ProcNum - 1)*n; i < N; i++)
 		{
 			TotalSum += vec[i];
 		}
-		
-		//Прием значений Sum, их сумма и вычисление среднего
-		for (int i = 1; i < ProcNum; i++) {
-			MPI_Recv(&Sum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &Status);
-			TotalSum += Sum;
-		}
-		
-				
+		cout << "ProcRank: " << ProcRank <<" Sum: "<< TotalSum << endl;
+		cout << endl;
 	}
 	else
-	{
-		cout << "ProcRank: " << ProcRank << endl;
-
+	{		
 		MPI_Recv(tmp, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &Status);//прием значений 
+		cout << "ProcRank: " << ProcRank << endl;
 		for (int i = 0; i < n; i++)
+		{
+			cout << " " << tmp[i];
 			Sum += tmp[i];
+		}
+		cout << " Sum: " << Sum << endl;
 		MPI_Send(&Sum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);// отправка Sum
-		cout << Sum << endl;
+		
 	}
 	if (ProcRank == 0)
 	{
-		TotalSum = TotalSum / N;
+		//Прием значений Sum, их сумма и вычисление среднего
+		for (int i = 1; i < ProcNum; i++) {
+			MPI_Recv(&Sum, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &Status);
+			TotalSum += Sum;
+		}
+		TotalSum /= N;
 		time2 = MPI_Wtime();
 		//Вывод
-		cout << "Total sum: " << TotalSum << endl;
+		cout << "Result: " << TotalSum << endl;
 		cout << "Runtime: " << time2 - time1 << endl;
+		system("pause");
 	}
-	
-	
+
+
 	MPI_Finalize();
-	cin.ignore();
-	cin.ignore();
+	
 	return 0;
 }
